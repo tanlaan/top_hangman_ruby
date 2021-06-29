af474aa5c9453d75da65ef2c1927d49a7022553c
Chris Bolas <tanlaan33@gmail.com>
2021-06-28 18:33:44 -0700

    Added tui file to handle text interface

--- hangman.rb
+++ hangman.rb
@@ -1,11 +1,12 @@
 require_relative './lib/hangmen.rb'
 require_relative './lib/game_logic.rb'
+require_relative './lib/tui.rb'
 
 def read_words_from_file()
     words = File.open('./5desk.txt', 'r')
     word_list = []
     words.each_line do |word|
-        word_list += [word.downcase] if word.length >= 5 && word.length <= 12
+        word_list += [word.downcase.chomp] if word.length >= 5 && word.length <= 12
     end
     word_list
 end
@@ -18,11 +19,12 @@ end
 def main()
     puts 'Loading...'
     words = read_words_from_file
+    # words = ['testing']
     playing = true
     start_message
     while playing
         Game.new(words).start
-        playing = play_again
+        playing = play_again_prompt
     end
 end
 
---
+++ lib/tui.rb
@@ -0,0 +1,18 @@
+# Textual User Interface functions
+def play_again_prompt
+    player_response = nil
+    while valid_response(player_response).nil?
+        puts "Want to play again?"
+        print ">"
+        player_response = gets.chomp
+    end
+    valid_response(player_response)
+end
+
+def valid_response(response)
+    valid_yes = ['yes','y']
+    valid_no = ['no', 'n']
+    return true if valid_yes.include?(response)
+    return false if valid_no.include?(response)
+    nil
+end
